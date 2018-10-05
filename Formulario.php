0ee34c0833e42181a1052b81e3deaab558ade6a8
Alejandro <Alejandro@Alejandro>
2018-10-05 16:44:12 -0500

    Alejandro Quijandria: cambiando el nombre a Listado.php

--- Formulario.php
+++ Formulario.php
@@ -3,7 +3,7 @@
   <title>Actividad</title>
  </head>
  <body>
-<form action="Lista.php" method="post">
+     <form action="Listado.php" method="post">
     <p>Cantidad: <input type="text" name="cantidad" placeholder="Ingresar Cantidad"/></p>
  <p><input type="submit" name="Mostrar" value="Mostrar" /></p>
 </form>
