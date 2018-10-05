22538cd85b55defc13124851b65b88fc9bb075a1
Quija <Quija@AlejandroII>
2018-10-05 16:38:56 -0500

    Lincol Ramirez:Creación del listado

---
+++ Listado.php
@@ -0,0 +1,16 @@
+<html>
+ <head>
+  <title>Lista</title>
+ </head>
+ <body>
+     
+     <h1>Lista de #s</h1>
+     <?php
+     include("Funcion.php");
+     
+
+        echo resultado();
+
+        ?>
+ </body>
+</html>
